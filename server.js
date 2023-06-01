3ca0d1bbabae46b6142e9cdf0d80d85eed632220
Nicolas Nueca <nicolasnueca@nicolass-air.lan>
2023-05-31 21:24:53 -0700

    sending index and show data

--- server.js
+++ server.js
@@ -6,14 +6,18 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
+// MIDDLEWARE
+const breadsController = require('./controllers/bread_controller.js')
+app.use('/breads', breadsController)
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+
 //routes
 app.get('/', (req, res) => {
     res.send('Welcome to an awesome app about breads!')
 })
-
-//breads
-const breadsController = require('./controllers/bread_controller.js')
-app.use('/breads', breadsController)
 
 //listen
 app.listen(PORT, () => {
