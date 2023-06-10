bf6fb599aed1039e42cd6afaec5b7f07b3f66ea6
Nicolas Nueca <nicolasnueca@Nicolass-MacBook-Air.local>
2023-06-10 10:55:11 -0700

    fixed errors with router

--- server.js
+++ server.js
@@ -5,15 +5,9 @@ const express = require('express')
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
+const methodOverride = require('method-override')
+const mongoose = require('mongoose')
 
-// MIDDLEWARE
-const breadsController = require('./controllers/bread_controller.js')
-app.use('/breads', breadsController)
-app.use(express.static('public'))
-app.use(express.urlencoded({extended: true}))
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
 
 
 //routes
@@ -21,11 +15,24 @@ app.get('/', (req, res) => {
     res.render('Welcome to my awesome Bread app')
 })
 
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
+
+mongoose.connect(process.env.MONGO_URI, 
+    {useNewUrlParser: true, useUnifiedTopology: true}, () => { 
+        console.log('connected to mongo: ', process.env.MONGO_URI) 
+    })
+        
 //404 Page
 app.get('*', (req, res) => {
     res.send('404')
 })
-
+        
 //listen
 app.listen(PORT, () => {
     console.log('listening on port', PORT)
